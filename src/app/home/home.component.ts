5d5251f66b09ad296263cf506f7206787f356ff2
Naveen Athirala <naveenkumarathirala97@gmail.com>
2021-09-01 06:54:23 +0530

    Single Page Applications Part-2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Promotion } from '../shared/promotion';
+import { Leader } from '../shared/leader';
 import { DishService } from '../services/dish.service';
 import { PromotionService } from '../services/promotion.service';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +15,14 @@ export class HomeComponent implements OnInit {
 
   dish: Dish ;
   promotion: Promotion;
+  leader: Leader;
 
-  constructor(private dishService: DishService, private promotionService: PromotionService) { }
+  constructor(private dishService: DishService, private promotionService: PromotionService, private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
+    this.dish = this.dishService.getDish("0");
+    this.promotion = this.promotionService.getPromotion("0");
+    this.leader = this.leaderService.getLeader("0");
 
   }
 
